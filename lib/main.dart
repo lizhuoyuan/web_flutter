d9104076048a0aefeb8622c8b10a64fdb9bf4511
李卓原 <zhuoyuan@cece.com>
2022-12-16 18:09:08 +0800

    test

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
         title: Text(widget.title),
       ),
